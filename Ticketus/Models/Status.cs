13772cc1c8705ac6f6353c25818c4403429f8f10
W2001-RF <rifaywassim@gmail.com>
2024-10-15 19:21:25 +0100

    Add Status model and link it with User modl

---
+++ Ticketus/Models/Status.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Ticketus.Models
+{
+    public class Status
+    {
+        [Key]
+        public int StatusId { get; set; }
+
+        [Required]
+        [MaxLength(50)]
+        public string StatusName { get; set; }
+    }
+}

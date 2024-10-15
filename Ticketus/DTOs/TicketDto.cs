0661a2fabd6b4950da1d49fb5b73e93fd66cdb92
W2001-RF <rifaywassim@gmail.com>
2024-10-15 10:06:55 +0100

    add Ticket DTO

---
+++ Ticketus/DTOs/TicketDto.cs
@@ -0,0 +1,10 @@
+﻿namespace Ticketus.DTOs
+{
+    public class TicketDto
+    {
+        public int TicketId { get; set; }
+        public string Description { get; set; }
+        public string Status { get; set; }
+        public DateTime DateCreated { get; set; }
+    }
+}

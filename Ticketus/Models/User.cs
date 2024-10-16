f076e0244d59ac6464226460ef7b76a93b6fdf81
W2001-RF <rifaywassim@gmail.com>
2024-10-16 02:41:31 +0100

    add password to User model

--- Ticketus/Models/User.cs
+++ Ticketus/Models/User.cs
@@ -16,6 +16,9 @@ namespace Ticketus.Models
         [MaxLength(255)]
         public string Email { get; set; }
 
+        public byte[] PasswordHash { get; set; }
+        public byte[] PasswordSalt { get; set; }
+
         public DateTime DateJoined { get; set; } = DateTime.Now;
 
         public List<Ticket> Tickets { get; set; } = new List<Ticket>();

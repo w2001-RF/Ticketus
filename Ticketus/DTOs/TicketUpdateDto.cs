d5bb07832b77ee08467c9f3d8263e72aff81b637
W2001-RF <rifaywassim@gmail.com>
2024-10-16 03:09:09 +0100

    add Update Ticket DTO

---
+++ Ticketus/DTOs/TicketUpdateDto.cs
@@ -0,0 +1,10 @@
+﻿namespace Ticketus.DTOs
+{
+    public class TicketUpdateDto
+    {
+        public string Description { get; set; }
+        public string Status { get; set; }
+        public int UserId { get; set; }
+        public DateTime DateCreated { get; set; } = DateTime.Now;
+    }
+}

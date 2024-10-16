46c3622b7461bc0621173b1f10ca1ed2ebe5b048
W2001-RF <rifaywassim@gmail.com>
2024-10-16 02:40:25 +0100

    add password update dto

---
+++ Ticketus/DTOs/PasswordUpdateDto.cs
@@ -0,0 +1,8 @@
+﻿namespace Ticketus.DTOs
+{
+    public class PasswordUpdateDto
+    {
+        public string CurrentPassword { get; set; }
+        public string NewPassword { get; set; }
+    }
+}

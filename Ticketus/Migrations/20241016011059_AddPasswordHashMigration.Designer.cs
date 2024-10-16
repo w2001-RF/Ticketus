5b986e781fd4e9013944868bd33b39c24d3bfe22
W2001-RF <rifaywassim@gmail.com>
2024-10-16 02:43:02 +0100

    add migration of adding password in th user table

--- Ticketus/Migrations/20241016011059_AddPasswordHashMigration.Designer.cs
+++ Ticketus/Migrations/20241016011059_AddPasswordHashMigration.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using Ticketus.Data;
 
@@ -11,9 +12,11 @@ using Ticketus.Data;
 namespace Ticketus.Migrations
 {
     [DbContext(typeof(TicketDbContext))]
-    partial class TicketDbContextModelSnapshot : ModelSnapshot
+    [Migration("20241016011059_AddPasswordHashMigration")]
+    partial class AddPasswordHashMigration
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ Ticketus/Migrations/20241016011059_AddPasswordHashMigration.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+#pragma warning disable CA1814 // Prefer jagged arrays over multidimensional
+
+namespace Ticketus.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddPasswordHashMigration : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DeleteData(
+                table: "Tickets",
+                keyColumn: "TicketId",
+                keyValue: 1);
+
+            migrationBuilder.DeleteData(
+                table: "Tickets",
+                keyColumn: "TicketId",
+                keyValue: 2);
+
+            migrationBuilder.DeleteData(
+                table: "Users",
+                keyColumn: "UserId",
+                keyValue: 1);
+
+            migrationBuilder.DeleteData(
+                table: "Users",
+                keyColumn: "UserId",
+                keyValue: 2);
+
+            migrationBuilder.AddColumn<byte[]>(
+                name: "PasswordHash",
+                table: "Users",
+                type: "varbinary(max)",
+                nullable: false,
+                defaultValue: new byte[0]);
+
+            migrationBuilder.AddColumn<byte[]>(
+                name: "PasswordSalt",
+                table: "Users",
+                type: "varbinary(max)",
+                nullable: false,
+                defaultValue: new byte[0]);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "PasswordHash",
+                table: "Users");
+
+            migrationBuilder.DropColumn(
+                name: "PasswordSalt",
+                table: "Users");
+
+            migrationBuilder.InsertData(
+                table: "Users",
+                columns: new[] { "UserId", "DateJoined", "Email", "UserName" },
+                values: new object[,]
+                {
+                    { 1, new DateTime(2024, 10, 15, 19, 10, 37, 160, DateTimeKind.Local).AddTicks(3905), "john.doe@example.com", "JohnDoe" },
+                    { 2, new DateTime(2024, 10, 15, 19, 10, 37, 160, DateTimeKind.Local).AddTicks(3914), "jane.smith@example.com", "JaneSmith" }
+                });
+
+            migrationBuilder.InsertData(
+                table: "Tickets",
+                columns: new[] { "TicketId", "DateCreated", "Description", "StatusId", "UserId" },
+                values: new object[,]
+                {
+                    { 1, new DateTime(2024, 10, 15, 19, 10, 37, 160, DateTimeKind.Local).AddTicks(3957), "Fix homepage bug", 1, 1 },
+                    { 2, new DateTime(2024, 10, 15, 19, 10, 37, 160, DateTimeKind.Local).AddTicks(3966), "Update contact page", 2, 2 }
+                });
+        }
+    }
+}

072557a4568fa792a81747cb52b484f9d09199c1
W2001-RF <rifaywassim@gmail.com>
2024-10-16 02:40:07 +0100

    add signup dto

---
+++ Ticketus/DTOs/SignupDto.cs
@@ -0,0 +1,9 @@
+﻿namespace Ticketus.DTOs
+{
+    public class SignupDto
+    {
+        public string UserName { get; set; }
+        public string Email { get; set; }
+        public string Password { get; set; }
+    }
+}

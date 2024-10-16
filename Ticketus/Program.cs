72de70e498694f7ce84de786c9f1dad42de5a4f1
W2001-RF <rifaywassim@gmail.com>
2024-10-16 02:44:18 +0100

    Add checking of jwt bear token as middleware

--- Ticketus/Program.cs
+++ Ticketus/Program.cs
@@ -1,14 +1,36 @@
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.IdentityModel.Tokens;
+using System.Text;
 using Ticketus.Data;
 using Microsoft.EntityFrameworkCore;
+using Ticketus.Services;
 
 var builder = WebApplication.CreateBuilder(args);
 
+// Add JWT Service
+builder.Services.AddScoped<JwtService>();
+
+// Add authentication using JWT Bearer tokens
+builder.Services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
+    .AddJwtBearer(options =>
+    {
+        options.TokenValidationParameters = new TokenValidationParameters
+        {
+            ValidateIssuer = true,
+            ValidateAudience = true,
+            ValidateLifetime = true,
+            ValidateIssuerSigningKey = true,
+            ValidIssuer = builder.Configuration["Jwt:Issuer"],
+            ValidAudience = builder.Configuration["Jwt:Audience"],
+            IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(builder.Configuration["Jwt:Key"]))
+        };
+    });
 // Add services to the container.
 
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
-builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
+//builder.Services.AddEndpointsApiExplorer();
+//builder.Services.AddSwaggerGen();
 
 //builder.Services.AddDbContext<TicketDbContext>(options => options.UseInMemoryDatabase("TicketDB"));
 builder.Services.AddDbContext<TicketDbContext>(options =>
@@ -19,12 +41,13 @@ var app = builder.Build();
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
-    app.UseSwagger();
-    app.UseSwaggerUI();
+    //app.UseSwagger();
+    //app.UseSwaggerUI();
 }
 
-app.UseHttpsRedirection();
+//app.UseHttpsRedirection();
 
+app.UseAuthentication();
 app.UseAuthorization();
 
 app.MapControllers();

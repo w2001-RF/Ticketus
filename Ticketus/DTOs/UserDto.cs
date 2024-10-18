70dcc96b9ecff4fe21470ae9da8f097f8d6930ca
W2001-RF <rifaywassim@gmail.com>
2024-10-18 03:57:19 +0100

    add User DTO

---
+++ Ticketus/DTOs/UserDto.cs
@@ -0,0 +1,11 @@
+﻿namespace Ticketus.DTOs
+{
+    public class UserDto
+    {
+        public int UserId { get; set; }
+        public string UserName { get; set; }
+        public string Email { get; set; }
+        public int TicketCount { get; set; }
+    }
+
+}
